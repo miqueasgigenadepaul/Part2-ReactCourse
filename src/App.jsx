c5c8a05d718254702070a4303bc228f32b317fa9
Miqueas <miqueasgigena18@gmail.com>
2025-01-01 15:52:56 -0300

    UNICAFE STEP 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import {useState} from 'react'
 
+// destructuring the properties of the object props
+const Statistics = ({good, neutral, bad, total, average, positive}) => {
+  
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}</p>
+    </div>
+  )
+}
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -15,16 +29,16 @@ const App = () => {
       <button onClick = {() => setGood(good + 1)}>good</button>
       <button onClick = {() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick = {() => setBad(bad + 1)}>bad</button>
-      
       <h1>statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {total}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}</p>
+      <Statistics 
+      good = {good}
+      neutral = {neutral}
+      bad = {bad}
+      average = {average}
+      positive = {positive}
+      />
     </div>
-  )
+    )
 }
 
-  export default App
+export default App
