4a1a4045cef324f588e35a9047218d8bb70054c2
Miqueas <miqueasgigena18@gmail.com>
2025-01-01 15:52:09 -0300

    UNICAFE STEP 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-const [good, setGood] = useState(0)
-const [neutral, setNeutral] = useState(0)
-const [bad, setBad] = useState(0)
+  const total = good + neutral + bad 
+  const average = (good * 1 + neutral * 0 + bad * (-1)) / total
+  const positive = good / total 
 
-return (
+  return (
     <div>
       <h1>give feedback</h1>
-        <button onClick = {() => setGood(good + 1)}>good</button>
-        <button onClick = {() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick = {() => setBad(bad + 1)}>bad</button>
-        <h1>statistics</h1>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
+      <button onClick = {() => setGood(good + 1)}>good</button>
+      <button onClick = {() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick = {() => setBad(bad + 1)}>bad</button>
+      
+      <h1>statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}</p>
     </div>
   )
 }
 
-export default App
+  export default App
