e033f1c62506b44852bcd7bbe6807a404baaf691
Miqueas <miqueasgigena18@gmail.com>
2025-01-08 23:57:51 -0300

    Phonebook step 1 add person

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,49 @@
-import Course from './components/Course'
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id : 1}
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      important: Math.random() < 0.5,
+      id: persons.length + 1,
     }
-  ]
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+  
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
-      <h1>Web development curriculum</h1>
-      <Course courses={courses} />
+      <h2>Phonebook</h2>
+      <form onSubmit = {addPerson}>
+        <div>
+          name: <input 
+                  value = {newName}
+                  onChange={handleInputChange}
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <li key = {person.id}>{person.name}</li>
+        )}
+      </ul>
+
+      
     </div>
   )
 }
