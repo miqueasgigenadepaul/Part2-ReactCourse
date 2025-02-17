f371ab467eeeeddb2e21259d66dd195ff8897438
Miqueas <miqueasgigena18@gmail.com>
2025-02-16 22:35:51 -0300

    uploading everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,75 @@
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course}</h1>
-}
-
-const Content = ({course}) => {
-  console.log(course)
-  return (
-    <div>
-      {course.parts.map(part => 
-        <div key = {part.id}>
-          {part.name} {part.exercises}
-        </div>)}
-    </div>
-  )
-}
-
-const Total = ({course}) => {
-  let sum = 0 
-  for (let i = 0; i < course.parts.length; i++) {
-    sum = sum + course.parts[i].exercises
-  }
-  return <p><b>total of {sum} exercises</b></p>
-}
-
-const Course = ({course}) => {
-  console.log(course)
-  return (
-    <div>
-      <Header course = {course.name}/>
-      <Content course = {course}/>
-      <Total course = {course}/>
-    </div>
-  )
-}
+import { useState, useEffect } from 'react'
+import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes)
+    })
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    }
+
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
   }
 
-  return <Course course={course} />
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`)
+        setNotes(notes.filter((n) => n.id !== id))
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
 }
 
 export default App
