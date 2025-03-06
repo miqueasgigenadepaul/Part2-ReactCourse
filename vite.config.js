93b1f504debdf8e195036696f15fb1250b0d865e
Miqueas <miqueasgigena18@gmail.com>
2025-03-05 22:51:28 -0300

    git feat: add base:'/' to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  base: "./",
+  
   server: {
     proxy: {
-      '/api': {
+      '/api/persons': {
         target: 'http://localhost:3001',
         changeOrigin: true,
       },
