c94768001b83acf1303701b26c17e64f5c20e83e
Miqueas <miqueasgigena18@gmail.com>
2025-03-06 00:11:36 -0300

    git feat: exercise 3.21 updated frontend

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  base: "./",
-  
+    
   server: {
     proxy: {
-      '/api/persons': {
+      '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
       },
